de3affad1130271abbc7c10fc7fbd232a34d5c43
Matthew Signorini <m.signorini@student.unimelb.edu.au>
2015-02-09 10:56:13 +1100

    Created a script to run prediction_script.m over all of our test cases, and compare the test results with the output of the original code.
    There are bugs still to be fixed!

---
+++ test_run.sh
@@ -0,0 +1,49 @@
+#!  /bin/bash
+#
+# Shell script to run the prediction program on every input file in the
+# samples/ directory, and compare the output to the results of the
+# original code (the two should match).
+
+output_dir="./spool"
+
+if test ! -e ${output_dir}
+then
+    mkdir ${output_dir}
+fi
+
+for input_file in ./samples/*
+do
+    base_name=`basename ${input_file}`
+    output_file="${output_dir}/${base_name}.out"
+    diff_file="${output_dir}/${base_name}.diff"
+    expected_output="output/original/${base_name}"
+
+    octave -q ./prediction_script.m -c 1 < ${input_file} > ${output_file}
+    diff -u ${expected_output} ${output_file} > ${diff_file}
+
+    # diff has a nonzero exit status if the two files differ.
+    if test $? != 0
+    then
+        failed_tests="${failed_tests} ${base_name}"
+        failed_diffs="${failed_diffs} ${diff_file}"
+    else
+        delete_list="${delete_list} ${output_file} ${diff_file}"
+    fi
+done
+
+/bin/rm -f ${delete_list}
+
+if test "${failed_tests}" != ""
+then
+    echo "The following samples did not match the original results:" > .diff
+    echo "${failed_tests}" >> .diff
+    echo "Diffs follow. Hit n to go to the next diff." >> .diff
+    less .diff ${failed_diffs}
+    /bin/rm -f .diff
+    exit 1
+else
+    echo "All outputs matched original."
+    exit 0
+fi
+
+# vim: ts=4 sw=4 et
