ebfac74bf1895ac2711725b2bb87c0a7d6dad018
Matthew Signorini <m.signorini@student.unimelb.edu.au>
2015-04-14 12:23:56 +1000

    Pretty printed connect.py. This is going to create a merge conflict, I just know it...

--- cloud/connect.py
+++ cloud/connect.py
@@ -4,20 +4,20 @@ import time
 from boto.ec2.regioninfo import RegionInfo
 
 # set region and keys
-region=RegionInfo(name='melbourne',endpoint='nova.rc.nectar.org.au')
-access_key='9d1c232e2fe443a8a29239fd113b359c'       
-access_secret='6dd011a67d0f4958945006572258bfc0'   
+region = RegionInfo (name = 'melbourne',endpoint = 'nova.rc.nectar.org.au')
+access_key = '9d1c232e2fe443a8a29239fd113b359c'       
+access_secret = '6dd011a67d0f4958945006572258bfc0'   
 
 # check if the instances and volumes are ready 
-def check_ready():
+def check_ready ():
     for instance in instances:
-        if instance.private_dns_name=='my-test':
-            status = instance.update()
+        if instance.private_dns_name == 'my-test':
+            status = instance.update ()
             x = 6
             while status != 'running':
-                time.sleep(10)
+                time.sleep (10)
                 x-=1
-                status = instance.update()
+                status = instance.update ()
                 if x < 0:
                     break
         else:
@@ -25,19 +25,25 @@ def check_ready():
 
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     # establish connection
-    connection=boto.connect_ec2(aws_access_key_id=access_key,aws_secret_access_key=access_secret,
-                          is_secure=True,region=region,port=8773,path='/services/Cloud',validate_certs=False)
-    reservations=connection.get_all_reservations()
+    connection = boto.connect_ec2 (aws_access_key_id = access_key,
+            aws_secret_access_key = access_secret,
+            is_secure = True,
+            region = region,
+            port = 8773,
+            path = '/services/Cloud',
+            validate_certs = False)
+
+    reservations = connection.get_all_reservations ()
     instances = [instance for r in reservations for instance in r.instances]
     
-    check_ready()
+    check_ready ()
     # get the id of each instance and volume 
     ips = ''
     for instance in instances:
         #print instance.private_dns_name
-        if instance.private_dns_name=='my-test':                   
+        if instance.private_dns_name == 'my-test':                   
             ips +=  instance.ip_address+','
     ips = ips[:-1]
     print ips
